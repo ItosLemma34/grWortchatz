10ff8ad73a03f4f8b12482850d96c0587326531d
ItosLemma34 <78398345+ItosLemma34@users.noreply.github.com>
2026-05-01 01:45:54 -0400

    Add files via upload

--- words/p.js
+++ words/p.js
@@ -1,82 +1,83 @@
 const p = [
-   'Pächter',  // Pächter: 
-   'paktieren', // paktieren: 
-   'Palme (auf die P. treiben/bringen)', //  duplicated  
-   'pampig',  // pampig: 
-   'Pappe, pappen, Papp-',  
-   'päppeln', // päppeln:   
-   'Pappenstiel', // Pappenstiel: 
-   'Parade (in die P. fahren)', //  duplicated 
-   'Parzelle',  // Parzelle:  
-   'Patrone', // Patrone: 
-   'patzen',  // patzen:
-   'patzig, Patzigkeit', 
-   'pauken', // pauken: 
-   'Pegel',  // Pegel: 
-   'peilen', // peilen: 
-   'peinigen, Peiniger', 
-   'Peitsche, peitschen', 
-   'Pelle (auf die P. rücken)', //  duplicated 
-   'pennen',  // pennen:
-   'perlen',  // perlen:
-   'Pest an den Hals', 
-   'petzen', // petzen: 
-   'Pfaffe', // Pfaffe: 
-   'Pfanne (in die P. hauen)',  // duplicated  
-   'Pfau',  // Pfau: 
-   'pfeffern, gepfeffert',  //  duplicated 
-   'Pfeiler',  // Pfeiler:  
-   'pferchen, ein-',   
-   'pfiffig',  // pfiffig: 
-   'Pfropfen',  // Pfropfen: 
-   'pfuschen, ins Handwerk p.', //  duplicated 
-   'Pfütze',  // Pfütze:
-   'piefig', // piefig:   
-   'pieksen, piksen', 
-   'pilgern',  // pilgern: 
-   'Pimpf',  // Pimpf: 
-   'Pinkel', // Pinkel: 
-   'pinkeln',  // pinkeln: 
-   'pinseln, bepinseln',  //  duplicated  
-   'Plackerei', // Plackerei:
-   'Plane',  // Plane:
-   'planschen', // planschen:  
-   'plappern, nach-, Geplapper',  // duplicated  
-   'platschen', // platschen: 
-   'plätschern', // plätschern: 
-   'Platzhirsch', // Platzhirsch: 
-   'ploppen',  // ploppen: 
-   'plump, plumpsen', 
-   'plüschig',  // plüschig:
-   'Pobacke',  // Pobacke: 
-   'pöbeln, Pöbel', 
-   'Polente', // Polente: 
-   'Polster, polstern, Polsterung',  
-   'poltern' ,  // poltern: 
-   'postieren', // postieren: 
-   'prallen, auf-, ab-',   
+  'Pächter',  // Pächter: 
+  'paktieren', // paktieren: 
+  'Palme (auf die P. treiben/bringen)', //  duplicated  
+  'pampig',  // pampig: 
+  'Pappe, pappen, Papp-',
+  'päppeln', // päppeln:   
+  'Pappenstiel', // Pappenstiel: 
+  'Parade (in die P. fahren)', //  duplicated 
+  'Parzelle',  // Parzelle:  
+  'Patrone', // Patrone: 
+  'patzen',  // patzen:
+  'patzig, Patzigkeit',
+  'pauken', // pauken: 
+  'Pegel',  // Pegel: 
+  'peilen', // peilen: 
+  'peinigen, Peiniger',
+  'Peitsche, peitschen',
+  'Pelle (auf die P. rücken)', //  duplicated 
+  'pennen',  // pennen:
+  'perlen',  // perlen:
+  'Pest an den Hals',
+  'petzen', // petzen: 
+  'Pfaffe', // Pfaffe: 
+  'Pfanne (in die P. hauen)',  // duplicated  
+  'Pfau',  // Pfau: 
+  'pfeffern, gepfeffert',  //  duplicated 
+  'Pfeiler',  // Pfeiler:  
+  'pferchen, ein-',
+  'pfiffig',  // pfiffig: 
+  'Pfropfen',  // Pfropfen: 
+  'pfuschen, ins Handwerk p.', //  duplicated 
+  'Pfütze',  // Pfütze:
+  'piefig', // piefig:   
+  'pieksen, piksen',
+  'pilgern',  // pilgern: 
+  'Pimpf',  // Pimpf: 
+  'Pinkel', // Pinkel: 
+  'pinkeln',  // pinkeln: 
+  'pinseln, bepinseln',  //  duplicated  
+  'Plackerei', // Plackerei:
+  'Plane',  // Plane:
+  'planschen', // planschen:  
+  'plappern, nach-, Geplapper',  // duplicated  
+  'platschen', // platschen: 
+  'plätschern', // plätschern: 
+  'Platzhirsch', // Platzhirsch: 
+  'ploppen',  // ploppen: 
+  'plump, plumpsen',
+  'plüschig',  // plüschig:
+  'Pobacke',  // Pobacke: 
+  'pöbeln, Pöbel',
+  'Polente', // Polente: 
+  'Polster, polstern, Polsterung',
+  'poltern',  // poltern: 
+  'postieren', // postieren: 
+  'prallen, auf-, ab-',
   // 'präparieren' ,
-   'Prämisse',  // Prämisse:
-   'prangen', // prangen: 
-   'Pranke',  // Pranke: 
-   'Präsent, präsent',   
-   'prasseln',  // prasseln: 
-   'preschen',  // preschen: 
-   'prickeln',  // prickeln:
-   'Primel, eingehen wie eine P.',  // duplicated 
-   'Prise' ,  // Prise: 
-   'Pritsche', // Pritsche: 
-   'Prothese', // Prothese: 
-   'Protz, protzen, protzig',  
-   'prusten',  // prusten:  
-   'pudern',  // pudern: 
-   'Pulle',  // Pulle: 
-   'pumpen, auf Pump kaufen', // duplicated
-   'Puppen (bis in die P.)', // duplicated 
-   'Puste, pusten, aus-',  
-   'Pute',  // Pute: 
-   'Putz', // Putz: 
-   'Putz (auf den P. hauen)', //  duplicated  
-  ];
+  'Prämisse',  // Prämisse:
+  'prangen', // prangen: 
+  'Pranke',  // Pranke: 
+  'Präsent, präsent',
+  'prasseln',  // prasseln: 
+  'preschen',  // preschen: 
+  'prickeln',  // prickeln:
+  'Primel, eingehen wie eine P.',  // duplicated 
+  'Prise',  // Prise: 
+  'Pritsche', // Pritsche: 
+  'Prothese', // Prothese: 
+  'Protz, protzen, protzig',
+  'prusten',  // prusten:  
+  'pudern',  // pudern: 
+  'Pulle',  // Pulle: 
+  'pumpen, auf Pump kaufen', // duplicated
+  'Puppen (bis in die P.)', // duplicated 
+  'purzeln',  // purzeln:
+  'Puste, pusten, aus-',
+  'Pute',  // Pute: 
+  'Putz', // Putz: 
+  'Putz (auf den P. hauen)', //  duplicated  
+];
 
 export default p;      
